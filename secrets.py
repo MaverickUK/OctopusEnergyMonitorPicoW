79877ac548bd08b41083a71c416b69c06a2a1415
Peter Bridger <maverickuk@gmail.com>
2023-01-07 13:10:43 +0000

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,13 @@
+secrets = {
+    # Wifi credentials
+    'ssid': '',
+    'password': '',
+    # Octopus Energy details
+    'account_number': '',
+    'api_key':'',
+    'auth_header': 'Basic XXXXXXXXXXXXXXXXXXXXXXXXXXXX', # Base64 encoded API key
+    'mpan': '', # Electricity
+    'mprn': '', # Gas
+    'elec_serial': '',
+    'gas_serial': '', 
+}
